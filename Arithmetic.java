dcde640fda91aa81357368675c4a2d39ea19aa93
uneebjaved <60681251+uneebjaved@users.noreply.github.com>
2020-02-05 18:54:37 -0800

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -3,9 +3,11 @@ public class ArithmeticExpressionEvaluator {
 	
 
 	public static void main(String[] args) {
-		String b = "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) ) ";
-		System.out.println(InfixToPostfix(b));
 		
+		String b = "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) ) ";
+		String p = InfixToPostfix(b);
+		StdOut.println(p);
+		StdOut.println(EvaluatePostfix(p));
 	}
 	
 	//( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) ) 
@@ -28,12 +30,19 @@ public class ArithmeticExpressionEvaluator {
     } 
 	
 	public static String InfixToPostfix(String item) {
+		
+		//( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) ) 
+		//1 2 3 + 4 5 * * +
+		//stack = ( + ( ( * ( 
+		//postfix = 1 2 3 + 4 5 *
 		//make val stack
 		//make oper stack
+		
 		String postfix = new String("");
 		Stack<String> stack = new Stack<>();
 		String[] words = item.split(" ");
-		for (int i =0; i<words.length;i++) {
+		
+		for (int i = 0; i<words.length;i++) {
 			if (!(words[i].equals("(") ||words[i].equals(")") || words[i].equals("+") ||words[i].equals("-") ||words[i].equals("*") ||words[i].equals("/") ||words[i].equals("^"))) {
 				postfix += words[i];
 			}
@@ -41,28 +50,70 @@ public class ArithmeticExpressionEvaluator {
 				stack.push(words[i]);
 			}
 			else if (words[i].equals(")")) {
-				while(!stack.isEmpty() && stack.peek().equals("(")) {
+				while(!stack.isEmpty() && !stack.peek().equals("(")) {
 					postfix += stack.pop();
+					
 				}
-				if (stack.isEmpty() && stack.peek().equals("(")) 
+				if (!stack.isEmpty() && stack.peek().equals("(")) 
 					stack.pop();
 			}
 			else {
 				while(!stack.isEmpty() && precedance(words[i])<= precedance(stack.peek())){
 					postfix +=stack.pop();
 				}
-			
 				stack.push(words[i]);
 			}
 		}
-		while(!stack.isEmpty()) {
-			if(!stack.peek().equals("("))
-				postfix += stack.pop();
-		}
+
 		return postfix;
  	}
-	public void EvaluatePostfix(String item) {
+	
+	public static double EvaluatePostfix(String item) {
+		Stack<String> vals = new Stack<>();
+		Stack<String> opr = new Stack<>();
+		String[] words = item.split("");
+		double v = 0;
+		String op = "";
 		
+		//123+45**+
+		//stack vals = 1 100 
+		//stack opr = 
+		//v = 5
+		//op = *
+		
+		for (int i = 0; i < words.length; i++) {
+			if (!(words[i].equals("+") ||words[i].equals("-") ||words[i].equals("*") ||words[i].equals("/") ||words[i].equals("^"))) {
+				vals.push(words[i]);
+			}
+			else {
+				opr.push(words[i]);
+				v = Double.parseDouble(vals.pop());
+				
+				op = opr.pop();
+				
+				if (op.equals("+")) {
+					v = Double.parseDouble(vals.pop()) + v;
+					vals.push(Double.toString(v));
+					
+				}
+				else if (op.equals("-")) {
+					v = Double.parseDouble(vals.pop()) - v;
+					vals.push(Double.toString(v));
+					}
+				else if (op.equals("*")) {
+					v = Double.parseDouble(vals.pop()) * v;
+					vals.push(Double.toString(v));
+				}
+				else if (op.equals("/")) {
+					v = Double.parseDouble(vals.pop()) / v;
+					vals.push(Double.toString(v));
+				}
+				
+			}
+			
+			
+		}
+		return v;
 	}
 	
 		
