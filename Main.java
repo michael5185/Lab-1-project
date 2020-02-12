2ed6d2ed83707a0fb2d6debed79c80c40ac96490
michael5185 <60332261+michael5185@users.noreply.github.com>
2020-02-11 21:17:52 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+import DLList.java
+public class Main
+{
+	public static void main(String[] args) {
+		DLList list = new DLList();
+		list.insertAtBeginning(7);
+		list.insertAtBeginning(6);
+		list.insertAtBeginning(5);
+		list.insertAtBeginning(4);
+		list.insertAtBeginning(3);
+		list.insertAtBeginning(2);
+		list.insertAtBeginning(1);
+		list.insertAtBeginning(0);
+		list.insertAtEnd(8);
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
+		System.out.println(list.get(2));
+		System.out.println(list.get(3));
+		System.out.println(list.get(4));
+		System.out.println(list.get(5));
+		System.out.println(list.get(6));
+		System.out.println(list.get(7));
+		System.out.println(list.getEnd());
+	}
+}
