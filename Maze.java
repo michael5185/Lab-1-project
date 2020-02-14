fd586185b6cc3313697b09dc868591515023fde1
uneebjaved <60681251+uneebjaved@users.noreply.github.com>
2020-02-13 23:20:39 -0800

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -1,12 +1,18 @@
+//5104232581
 import java.util.Random;
 
-
+//The Maze class builds an explicit version of a grid that will be used by functions like union, find, connected, and destroyed
 public class Maze {
 	int size;
 	private Node[][] maze;
+	//The constructor below is what takes in how big the grid should be
 	Maze(int x){
 		size = x;
 	}
+	//randomNode takes in the size of the grid (its a formality)
+	//we get a random number from Random() and initialize rand to it.
+	//our coordinates x and y are chosen at random and are given values between 0-9. not 10 because 0 is considered as our first point and so 9 will be considered our 10th.
+	//the overall output of this function is that we get a node with coordinates (x,y), which is chosen at random from the 10x10 grid maze we have made
 	public Node randomNode(int size) {
 		Random rand = new Random();
 		int x = Math.abs(rand.nextInt()%size);
@@ -14,17 +20,21 @@ public class Maze {
 		int y = Math.abs(rand.nextInt()%size);
 		return maze[y][x];
 	}
+	//this functions whole idea is to make a 10x10 grid. this is made using the idea that a Node represents a coordinate in our grid and that all the Nodes are connected to other nodes as parents
 	public void make() {
 		maze = new Node[size][size];
 		for(int i =0; i<size;i++) {
 			for(int j=0;j<size;j++) {
 				maze[i][j] = new Node(i,j);
-				//System.out.print(i + "" + j);
-				//System.out.print(" ");
+
 			}
 			System.out.println();
 		}
 	}
+	//this function is a different implementation of find in the quick find algorithm. instead of returning the value of an index, this find takes in a Node (which is a point or a coordinate in our grid)
+	//and if the parent of the node is not the same as itself (meaning it doesn't have a parent/it is the parent of its node) then it returns the node
+	//the whole idea of this is to check if the node is connected to other nodes. because the way i built nodes are like the ones in a list, all nodes are connected in respect to parents(previous nodes)
+	//so 
 	public Node find(Node n) {
 		while(n.parent != n) {
 			n=n.parent;
@@ -42,12 +52,13 @@ public class Maze {
 		Node first = find(a);
         Node second = find(b);
         if(first == second){
+
             return false;
         }
         first.parent = second;
-	System.out.print("("a.x + " " + a.y+")");
+    	System.out.print("("+a.x + " " + a.y+")");
 		System.out.print(", ");
-	System.out.print("("b.x + " " + b.y+")");
+		System.out.print("("+b.x + " " + b.y+")");
         System.out.println();
         a.breakWall(b);
         return true;
