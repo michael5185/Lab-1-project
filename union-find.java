6f55a4d65a6d6f32dfa3af3a84916085f651be99
michael5185 <60332261+michael5185@users.noreply.github.com>
2020-02-13 20:54:30 -0800

    Update union-find.java

--- union-find.java
+++ union-find.java
@@ -1,8 +1,12 @@
-
+/****************************************
+* CIS27 Lab 1 Q3 Union-Find - Maze      *
+* -Main Function			*
+* Participant : Uneeb Javed, Taeho Lee  *
+*****************************************/
 public class unionfind {
 
 	public static void main(String[] args) {
-		Maze a = new Maze(150);
+		Maze a = new Maze(10);
 		a.run();
 	}
 }
