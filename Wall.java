e6649b221c4b15f1572b2dd6279e39d9aa66b790
michael5185 <60332261+michael5185@users.noreply.github.com>
2020-02-13 23:09:27 -0800

    Update Wall.java

--- Wall.java
+++ Wall.java
@@ -7,6 +7,7 @@ public class Wall {
     boolean top, down, right, left;
     
     Wall(){
+        //represents whether the wall in each cell is broken or not. if wall still exist, it returns false. if it's broken, it returns true
         top = false;
         down = false;
         right = false;
